88c7f3806cf6a9c6f90fee49d3fa5cd12fd72d90
Bayezid Mostafa <hiperx1@gmail.com>
2022-11-08 23:44:08 +0600

    Checked Service Data From Server

--- index.js
+++ index.js
@@ -3,10 +3,31 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Middleware
 app.use(cors());
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tikoekt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async () => {
+    try {
+        const serviceCollection = client.db('guiderDB1').collection('services');
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray()
+            res.send(services);
+        })
+    }
+    finally { }
+}
+run().catch(err => console.error(err));
+
+
+
 
 app.get('/', (req, res) => {
     res.send("My Pro's Guide server is running");
