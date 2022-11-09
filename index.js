29ccfd25f5b689f947dc836dc6b67f8048fe9532
Bayezid Mostafa <hiperx1@gmail.com>
2022-11-09 20:43:42 +0600

    Post Method Implemented For Reviews

--- index.js
+++ index.js
@@ -22,22 +22,27 @@ const run = async () => {
         })
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const service = await serviceCollection.findOne(query);
             res.send(service);
         })
-        app.get('/reviews', async(req, res) => {
+        app.get('/reviews', async (req, res) => {
             let query = {};
             console.log(req.query);
-            if(req.query.itemID){
+            if (req.query.serviceName) {
                 query = {
-                    itemId: req.query.itemID
+                    serviceName: req.query.serviceName
                 }
             }
             console.log(query);
             const cursor = reviewCollection.find(query);
             const review = await cursor.toArray()
             res.send(review)
+        })
+        app.post('/reviews', async (req, res) => {
+            const service = req.body;
+            const result = await reviewCollection.insertOne(service);
+            res.send(result);
         })
     }
     finally { }
